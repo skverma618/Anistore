728bb2f2aac4ab6e2da62ea69bc576d4f73f2857
Sushil <sushilkumarskp618@gmail.com>
2025-01-22 23:34:47 +0530

    router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,16 +2,44 @@
 import ProductCard from "./components/ProductCard1"
 import Navbar from "./components/custom/Header/Navbar"
 import ProductListing from "./layouts/ProductListing"
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
 
 function App() {
 
-  return (
-    <>
+  // Layout Component
+  const Layout = () => (
+    <div>
       <Navbar />
-      <section className="">
-        {/* <ProductListing /> */}
-      </section>
-    </>
+      <Outlet />
+    </div>
+  );
+
+  // Page Components
+  const Shop = () => <div>Shop Page</div>;
+  const Blog = () => <div>Blog Page</div>;
+  const About = () => <div>About Page</div>;
+  const Contact = () => <div>Contact Page</div>;
+  const LoginSignUp = () => <div>Login/Sign Up Page</div>;
+  const Cart = () => <div>Cart Page</div>;
+
+  // Define Routes
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Layout />,
+      children: [
+        { path: '/', element: <ProductListing /> },
+        { path: 'shop', element: <Shop /> },
+        { path: 'blog', element: <Blog /> },
+        { path: 'about', element: <About /> },
+        { path: 'contact', element: <Contact /> },
+        { path: 'loginSignUp', element: <LoginSignUp /> },
+        { path: 'cart', element: <Cart /> },
+      ],
+    },
+  ]);
+  return (
+    <RouterProvider router={router} />
   )
 }
 
